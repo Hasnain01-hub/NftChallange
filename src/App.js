48571ea8a2d4f019236c92b299410083ca1771f4
Hasnain01-hub <hasnainsayyed833@gmail.com>
2021-12-14 00:13:24 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Punklist from "./components/Punklist";
 import Main from "./components/Main";
-axios.defaults.headers.post['Content-Type'] = 'text/plain';
+ 
 
 function App() {
   const [data, setdata] = useState([]);
   const [select, setselect] = useState(0);
   useEffect(() => {
     const getdata = async () => {
+      var config = {
+        headers: {'Access-Control-Allow-Origin': '*'}
+    };
       const openseadata = await axios.get(
-        "https://testnets-api.opensea.io/assets?asset_contract_address=0x20178A5E1260779521e5F8594341961aD64C61CD&order_direction=asc",{'Access-Control-Allow-Credentials':true}
-        
-      );
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0x20178A5E1260779521e5F8594341961aD64C61CD&order_direction=asc",config);
       console.log(openseadata.data.assets);
       setdata(openseadata.data.assets)
       
