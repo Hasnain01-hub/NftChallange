74163add853330766a8af2b190b49905364ff159
Hasnain01-hub <hasnainsayyed833@gmail.com>
2021-12-14 12:46:36 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import "./App.css";
 import "./components/Header.css";
 import Header from "./components/Header";
@@ -7,46 +6,45 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Punklist from "./components/Punklist";
 import Main from "./components/Main";
- 
 
 function App() {
   const [data, setdata] = useState([]);
   const [select, setselect] = useState(0);
   useEffect(() => {
     const getdata = async () => {
-    const URL='https://testnets-api.opensea.io/assets?asset_contract_address=0x20178A5E1260779521e5F8594341961aD64C61CD&order_direction=asc';
-       await axios.get(URL, {
-        // method: 'GET',
-        headers: {
-          'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8;application/json',
-          
-        },
-        // data: exp,
-      })
-        .then(response => {setdata(response.data.assets); console.log(response.data.assets);})
-        .catch(error => {
+      const URL =
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0x20178A5E1260779521e5F8594341961aD64C61CD&order_direction=asc";
+      await axios
+        .get(URL, {
+          // method: 'GET',
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:'0xb784c1d5ae5b7e9640e1ab6626adf58f8bd5ec14'
+          },
+          // data: exp,
+        })
+        .then((response) => {
+          setdata(response.data.assets);
+          console.log(response.data.assets);
+        })
+        .catch((error) => {
           throw console.log(error);
         });
       // console.log(openseadata.data.assets);
-      
-      
     };
-    return getdata()
+    return getdata();
   }, []);
 
   return (
     <div className="app">
       <Header />
-      
-      {
-        data.length > 0  && ( 
-          <>
-        <Main select={select} punklistdata={data}/>
-          <Punklist 
-           punklistdata={data} selectedpunk={setselect}/>
-           </>
-           )}
-      
+
+      {data.length > 0 && (
+        <>
+          <Main select={select} punklistdata={data} />
+          <Punklist punklistdata={data} selectedpunk={setselect} />
+        </>
+      )}
     </div>
   );
 }
